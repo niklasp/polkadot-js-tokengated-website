d10a58a847653931165131ee7803773527834b0b
Niklas P <contact@niklasplessing.net>
2023-04-21 12:36:03 +0200

    remove unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import { PolkadotExtensionProvider } from '@/context/polkadotExtensionContext'
-import { usePolkadotExtension } from '@/hooks/usePolkadotExtension'
 
 export default function App(
   { 

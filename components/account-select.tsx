d35f28f9dca3b027651c2383fbad19d641970fe6
Niklas P <contact@niklasplessing.net>
2023-05-14 12:54:46 +0200

    format

--- components/account-select.tsx
+++ components/account-select.tsx
@@ -2,64 +2,55 @@ import { useState } from 'react';
 import Identicon from '@polkadot/react-identicon';
 import { usePolkadotExtensionWithContext } from '@/context/polkadotExtensionContext';
 
-import 'primereact/resources/themes/md-dark-indigo/theme.css'
-import 'primereact/resources/primereact.min.css'    
-import { Dropdown } from 'primereact/dropdown'
-import styles from '@/styles/Home.module.css'
-
+import 'primereact/resources/themes/md-dark-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import { Dropdown } from 'primereact/dropdown';
+import styles from '@/styles/Home.module.css';
 
 export const accountValueTemplate = (option: any, props: any) => {
-    if ( option ) {
+  if (option) {
     return (
-        <div className={ styles.accountOption }>
-            <div>
-                <Identicon
-                    value={option?.address}
-                    size={32}
-                    theme='polkadot'
-                />
-                {option?.meta?.name }
-            </div>
+      <div className={styles.accountOption}>
+        <div>
+          <Identicon value={option?.address} size={32} theme="polkadot" />
+          {option?.meta?.name}
         </div>
+      </div>
     );
-    }
+  }
 
-    return <span>{props.placeholder}</span>;
+  return <span>{props.placeholder}</span>;
 };
 
 export const accountOptionTemplate = (option: any) => {
-    return (
-        <div className={ styles.accountOption }>
-            <div>
-                <Identicon
-                    value={option?.address}
-                    size={32}
-                    theme='polkadot'
-                />
-                {option?.meta?.name }
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.accountOption}>
+      <div>
+        <Identicon value={option?.address} size={32} theme="polkadot" />
+        {option?.meta?.name}
+      </div>
+    </div>
+  );
 };
 
-export default function AccountSelector( ) {
-    const { accounts, actingAccount, setActingAccountIdx } = usePolkadotExtensionWithContext()
+export default function AccountSelector() {
+  const { accounts, actingAccount, setActingAccountIdx } = usePolkadotExtensionWithContext();
 
-    const [accounts,setAccounts] = useState([])
-
-    return (
-        <Dropdown 
-            options={ accounts ?? undefined }
-            optionLabel="address" 
-            placeholder="Select Account"
-            className={ styles.dropdown }
-            value={ actingAccount }
-            itemTemplate={ accountOptionTemplate }
-            valueTemplate={ accountValueTemplate }
-            onChange={(event) => {
-                const accountIdx = accounts ? accounts.findIndex((account) => account.address === event.target.value.address) : 0
-                setActingAccountIdx(accountIdx)
-            }}
-        />
-    )
+  return (
+    <Dropdown
+      options={accounts ?? undefined}
+      optionLabel="address"
+      placeholder="Select Account"
+      className={styles.dropdown}
+      value={actingAccount}
+      itemTemplate={accountOptionTemplate}
+      valueTemplate={accountValueTemplate}
+      onChange={(event) => {
+        const accountIdx = accounts
+          ? accounts.findIndex((account) => account.address === event.target.value.address)
+          : 0;
+        setActingAccountIdx(accountIdx);
+      }}
+    />
+  );
 }
--- components/polkadot-particles.tsx
+++ components/polkadot-particles.tsx
@@ -1,62 +1,55 @@
-import type { Container, Engine } from "tsparticles-engine";
-import Particles from "react-particles";
-import { loadConfettiPreset } from "tsparticles-preset-confetti";
-import { loadFull } from "tsparticles";
-import { useCallback } from "react"
+import type { Container, Engine } from 'tsparticles-engine';
+import Particles from 'react-particles';
+import { loadConfettiPreset } from 'tsparticles-preset-confetti';
+import { loadFull } from 'tsparticles';
+import { useCallback } from 'react';
 
 export default function PolkadotParticles() {
-
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadConfettiPreset(engine);
     await loadFull(engine);
   }, []);
-  
-  const particlesLoaded = useCallback(async (container: Container | undefined) => {
-    await console.log(container);
-  }, []);
 
-    return (
-
+  return (
     <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          particles: {
-            // add circle shape emitter
-            shape: {
-              type: "circle",
-            },
-            color: {
-              value: ["#E6007A", "#552BBF", "#00B2FF"],
-            },
-            size: {
-              value: { min: 5, max: 10 }
-            },
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        particles: {
+          // add circle shape emitter
+          shape: {
+            type: 'circle',
           },
-          emitters: {
-            "startCount": 200,
-            "position": {
-              "x": 50,
-              "y": 50
-            },
-            "size": {
-              "width": 30,
-              "height": 30,
-            },
-            "rate": {
-              "delay": 0,
-              "quantity": 0
-            },
-            life: {
-              count: 1,
-              duration: 3,
-              delay: 0
-            },
+          color: {
+            value: ['#E6007A', '#552BBF', '#00B2FF'],
           },
-          duration: 9,
-          preset: "confetti",
-        }}
-      />
-    )
+          size: {
+            value: { min: 5, max: 10 },
+          },
+        },
+        emitters: {
+          startCount: 200,
+          position: {
+            x: 50,
+            y: 50,
+          },
+          size: {
+            width: 30,
+            height: 30,
+          },
+          rate: {
+            delay: 0,
+            quantity: 0,
+          },
+          life: {
+            count: 1,
+            duration: 3,
+            delay: 0,
+          },
+        },
+        duration: 9,
+        preset: 'confetti',
+      }}
+    />
+  );
 }

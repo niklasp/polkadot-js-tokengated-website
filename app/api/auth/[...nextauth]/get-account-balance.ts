97b76b9ba704e8a7742db43f5fb9caa4beee0986
Niklas P <contact@niklasplessing.net>
2025-03-10 17:45:58 +0100

    add papi descriptiors

--- app/api/auth/[...nextauth]/get-account-balance.ts
+++ app/api/auth/[...nextauth]/get-account-balance.ts
@@ -1,8 +1,5 @@
-// `dot` is the name we gave to `npx papi add`
 import { dot } from '@polkadot-api/descriptors';
 import { createClient } from 'polkadot-api';
-// import from "polkadot-api/ws-provider/node"
-// if you are running in a NodeJS environment
 import { getWsProvider } from 'polkadot-api/ws-provider/web';
 import { withPolkadotSdkCompat } from 'polkadot-api/polkadot-sdk-compat';
 
